faaed202e0e2017a88f2491308fe92f390090e59
Vinh Nguyen <nguyenlebaovinh.27@gmail.com>
2023-02-01 13:15:27 -0600

    Complete JS functions continued

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`
+}
+saturdayFun()
+saturdayFun('bathe my dog')
+
+function mondayWork(activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`
+}
+
+mondayWork()
+mondayWork('work from home')
+
+function wrapAdjective(visual = '*') {
+    return function (adjective = 'special') {
+        return `You are ${visual}${adjective}${visual}!`
+    }
+
+}
+
+// function innerFunction(adjective = 'special') {
+//     return `You are ${adjective}!`
+// }
+
+wrapAdjective()
+wrapAdjective('||')
